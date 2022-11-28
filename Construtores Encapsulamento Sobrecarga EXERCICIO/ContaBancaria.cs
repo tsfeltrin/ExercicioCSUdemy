07c96698cc5dad82686ef27325be98eb0c194eea
Tais <tsfeltrin@gmail.com>
2022-11-27 22:16:26 -0300

    Inserindo no github

---
+++ Construtores Encapsulamento Sobrecarga EXERCICIO/ContaBancaria.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Construtores_Encapsulamento_Sobrecarga_EXERCICIO
+{
+    internal class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo{ get; private set; }
+
+        public ContaBancaria(int numero, string tiular) // construtores
+        {
+            Numero = numero;
+            Titular = tiular;
+        }
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this (numero, titular) // construtores
+        {
+            Deposito(depositoInicial); // quando for executado alguma ação no método abaixo Deposito, automaticamente engloba aquiesta mudança.
+        }
+
+        public void Deposito (double quantia)
+        {
+            Saldo += quantia;
+        }
+        public void Saque (double quantia)
+        {
+            Saldo -= quantia;
+            Saldo -= 5.00;
+        }
+        public override string ToString()
+        {
+            return "Conta " 
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Construtores Encapsulamento Sobrecarga EXERCICIO/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtores_Encapsulamento_Sobrecarga_EXERCICIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S') // caso tenha um saldo inicial
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.Write("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.Write("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
